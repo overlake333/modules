29b50948028999b9901e72972ce97356fd0d779d
James Fleming <Jamesdfleming1@gmail.com>
2019-10-07 20:08:32 -0400

    updating makefile and adding testhOpen.c

---
+++ testhOpen.c
@@ -0,0 +1,19 @@
+/* testhOpen.c --- 
+ * 
+ * 
+ * Author: James D. Fleming
+ * Created: Mon Oct  7 19:53:57 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include "hash.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void){
+
+	exit(EXIT_SUCCESS);
+}
